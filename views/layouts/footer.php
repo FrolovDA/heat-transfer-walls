83e35a6f75d2cc2639b9b6b281893b351507843d
Dmitry Frolov <mail@dafrolov.ru>
2019-07-16 09:14:39 +0400

    Created a template, added Bootstrap 4 and jQuery 3.4.1

--- views/layouts/footer.php
+++ views/layouts/footer.php
@@ -2,5 +2,8 @@
     <footer>
         Footer
     </footer>
+    <script src="/template/js/jquery-3.4.1.min.js"></script>
+    <script src="/template/js/popper.min.js"></script>
+    <script src="/template/js/bootstrap.min.js"></script>
 </body>
 </html>
--- views/layouts/header.php
+++ views/layouts/header.php
@@ -3,6 +3,9 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="/template/css/bootstrap.min.css">
+    <link href="/template/css/sticky-footer-navbar.css" rel="stylesheet">
+    <link rel="stylesheet" href="/template/css/style.css">
     <title>Title</title>
 </head>
 <body>
