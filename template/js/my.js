d8c6d3376fa54dfef438471e7052fe592d9f7dc4
Dmitry Frolov <mail@dafrolov.ru>
2019-07-16 10:22:00 +0400

    Added MathJax

--- template/js/my.js
+++ template/js/my.js
@@ -1,6 +1,16 @@
+/*
+ * Set class for active link
+ */
 let part = location.href.split('/');
 let request = part[3];
 if (request == "") {
     request = "index";
 }
 $("#link-" + request).addClass("active");
+
+/*
+ * Config MathJax
+ */
+MathJax.Hub.Config({
+    tex2jax: {inlineMath: [["$","$"],["\\(","\\)"]]}
+});
--- views/layouts/footer.php
+++ views/layouts/footer.php
@@ -5,6 +5,7 @@
     <script src="/template/js/jquery-3.4.1.min.js"></script>
     <script src="/template/js/popper.min.js"></script>
     <script src="/template/js/bootstrap.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.5/MathJax.js?config=TeX-MML-AM_CHTML"></script>
     <script src="/template/js/my.js"></script>
 </body>
 </html>
